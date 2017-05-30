2eb4d0eb78a093a8209be36c7041e2cdf498581a
Alessandro Bartsch <alebartsch@hotmail.com>
2017-05-30 11:27:36 +0200

    tried, but points arent correct

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -22,7 +22,9 @@
 
 from pid import PIDAgent
 from keyframes import hello
+import numpy as np
 
+eps = 1e-6
 
 class AngleInterpolationAgent(PIDAgent):
     def __init__(self, simspark_ip='localhost',
@@ -31,6 +33,7 @@ class AngleInterpolationAgent(PIDAgent):
                  player_id=0,
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.start = self.perception.time
         self.keyframes = ([], [], [])
 
     def think(self, perception):
@@ -41,6 +44,59 @@ class AngleInterpolationAgent(PIDAgent):
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        
+        time = self.perception.time - self.start
+        (names, times, keys) = keyframes
+        
+        for i, name in enumerate(names):
+            lTimes = times[i]
+            
+            if time<lTimes[0] or lTimes[-1]<time:
+                continue
+            
+            #get keys
+            eIndex = len([x for x in lTimes if x<time])
+            sIndex = eIndex - 1
+            ekeys = keys[i][eIndex]
+            skeys = keys[i][sIndex]
+            
+            # direct points
+            (p0x, p0y) = (lTimes[sIndex], skeys[0])
+            (p3x, p3y) = (lTimes[eIndex], ekeys[0])
+            # direction points
+            (p1x, p1y) = (p0x + skeys[2][1], p0y + skeys[2][2])
+            (p2x, p2y) = (p3x + ekeys[1][1], p3y + ekeys[1][2])
+            
+            # bezier
+            bezierMatrix = np.array([[1,0,0,0],[-3,3,0,0],[3,-6,3,0],[-1,3,-3,1]])
+            x = np.array([p0x,p1x,p2x,p3x])
+            y = np.array([p0y,p1y,p2y,p3y])
+            
+            #get solutions within error margin
+            coefficientsX = np.dot(bezierMatrix, x)
+            coefficientsX[0] -= time
+            solutions = np.polynomial.polynomial.polyroots(coefficientsX)
+            
+            solutions = [x.real for x in solutions if -(eps)<=x.real<=1+(eps) and x.imag == 0] 
+            
+            #possible more solutions cause errror margin
+            if len(solutions) > 1: 
+                 solutions = np.asarray([(x,np.abs(x-0.5)) for x in solutions],dtype = [("value", float),("distance", float)]) # closest to 0.5 (center of [0,1])
+                 solutions = np.sort(solutions, order="distance") #sorts in ascending order according to distance to 0.5
+                 t = solutions[0][0]
+            else: #one solution
+                t = solutions[0]
+          
+            # t in [0,1]
+            if t < 0.: 
+                t = 0.
+            if t > 1.: 
+                t = 1.
+              
+            #results
+            coefficientsY = np.dot(bezierMatrix, y)
+            result = np.dot(np.array([1, t, t**2, t**3]),coefficientsY)
+            target_joints[name] = result
 
         return target_joints
 
